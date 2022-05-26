bda9cb351371a4db6e44998e033271273a857784
azanniel <leo.azannielttt@gmail.com>
2022-05-26 12:18:23 -0400

    create app with react native CLI

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {Home} from './src/pages/Home';
+
+export default function App() {
+  return (
+    <>
+      <Home />
+
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+    </>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
+
+import {Button} from '../components/Button';
+import {SkillCard} from '../components/SkillCard';
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    if (newSkill.trim()) {
+      setMySkills(oldState => [...oldState, newSkill]);
+      setNewSkill('');
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome Leandro</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        value={newSkill}
+        onChangeText={setNewSkill}
+      />
+
+      <Button onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, {marginVertical: 40}]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({item}) => <SkillCard skill={item} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+});
